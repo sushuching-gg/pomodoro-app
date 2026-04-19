706bb36e4017d8ab433ae20102e7b3a674481186
sushuching-gg <sushuching.gg@gmail.com>
2026-04-19 10:32:47 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'pomodoro-v2';
+﻿const CACHE_NAME = 'pomodoro-v3'; // 提升版本號以強制更新
 const ASSETS = [
   './index.html',
   './manifest.json',
@@ -8,6 +8,17 @@ const ASSETS = [
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting(); // 強制跳過等待，立即生效
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
 });
 
